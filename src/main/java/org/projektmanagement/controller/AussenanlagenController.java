70f61767c72de8481abb7e2df7492bccdbd65d6c
Toan <Toan@172.22.180.186>
2018-01-05 10:05:03 +0100

    Aussenanlagen von Toan

--- src/main/java/org/projektmanagement/controller/AussenanlagenController.java
+++ src/main/java/org/projektmanagement/controller/AussenanlagenController.java
@@ -1,211 +1,232 @@
 package org.projektmanagement.controller;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import org.projektmanagement.model.Haus;
-import org.projektmanagement.model.HausSonderwunsch;
 import org.projektmanagement.model.Kunde;
+import org.projektmanagement.service.KundenService;
 import org.projektmanagement.service.SonderwunschService;
-import org.projektmanagement.utils.CSVExporter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+/*import java.io.FileWriter;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
-import javafx.stage.Stage;
-import javafx.scene.control.CheckBox;
+import java.util.ResourceBundle;*/
+
+import org.projektmanagement.utils.CSVExporter;
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-public class AussenanlagenController {
-
-	@FXML
-	private CheckBox cbAbstellraum;
-	@FXML
-	private CheckBox cbVorbereitungEAntriebMarkiseEG;
-	@FXML
-	private CheckBox cbVortbereitungEAntriebMarkiseDG;
-	@FXML
-	private CheckBox cbMarkiseEG;
-	@FXML
-	private CheckBox cbMarkiseDG;
-	@FXML
-	private CheckBox cbEAntriebGaragentor;
-	@FXML
-	private CheckBox cbSektionaltor;
+public class AussenanlagenController /*implements Initializable*/ {
 	private Stage stage;
-	private Kunde kunde;
-	private static final Logger log = LoggerFactory.getLogger(GrundrissController.class);
-	private SonderwunschService sonderwunschService = new SonderwunschService();
-
-	public void init(Stage stage) {
-		this.stage = stage;
-	}
-
-	public void setKunde(Kunde kunde) {
-		this.kunde = kunde;
-	}
-
-	// Zeige Sonderwünsche an, die bereits in der Datenbank vorhanden sind
-	public void checkIfExist() {
-		for (HausSonderwunsch s : sonderwunschService.getSonderwunschHandler().getSonderwunscheHouse(kunde)) {
-			if (s.getSonderwunsch().getId() == 44) {
-				cbAbstellraum.setDisable(false);
-				cbAbstellraum.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 45) {
-				cbVorbereitungEAntriebMarkiseEG.setDisable(false);
-				cbVorbereitungEAntriebMarkiseEG.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 46) {
-				cbVortbereitungEAntriebMarkiseDG.setDisable(false);
-				cbVortbereitungEAntriebMarkiseDG.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 47) {
-				cbMarkiseEG.setDisable(false);
-				cbMarkiseEG.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 48) {
-				cbMarkiseDG.setDisable(false);
-				cbMarkiseDG.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 49) {
-				cbEAntriebGaragentor.setDisable(false);
-				cbEAntriebGaragentor.setSelected(true);
-			}
-
-			if (s.getSonderwunsch().getId() == 50) {
-				cbSektionaltor.setDisable(false);
-				cbSektionaltor.setSelected(true);
-			}
-		}
-	}
-
-	// TODO: Must be modified if a GUI exists.
+	
+	private double dOpt0, dOpt1, dOpt2, dOpt3, dOpt4, dOpt5, dOpt6;
 	@FXML
-	protected void OnClickSave(ActionEvent event) {
-		Haus haus = kunde.getHouses().get(0);
-
-		// Lösche Sonderwünsche, die bereits in der Datenbank vorhanden sind und abgewählt wurden
-		for (HausSonderwunsch s : sonderwunschService.getSonderwunschHandler().getSonderwunscheHouse(kunde)) {
-			if (!this.cbAbstellraum.isSelected() && s.getSonderwunsch().getId() == 44) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(44));
-				log.info("Special request {} removed", cbAbstellraum.getText().replace(":", ""));
-			}
-			if (!this.cbVorbereitungEAntriebMarkiseEG.isSelected() && s.getSonderwunsch().getId() == 45) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(45));
-				log.info("Special request {} removed", cbVorbereitungEAntriebMarkiseEG.getText().replace(":", ""));
-			}
-			if (!this.cbVortbereitungEAntriebMarkiseDG.isSelected() && s.getSonderwunsch().getId() == 46) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(46));
-				log.info("Special request {} removed", cbVortbereitungEAntriebMarkiseDG.getText().replace(":", ""));
-			}
-			if (!this.cbMarkiseEG.isSelected() && s.getSonderwunsch().getId() == 47) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(47));
-				log.info("Special request {} removed", cbMarkiseEG.getText().replace(":", ""));
-			}
-			if (!this.cbMarkiseDG.isSelected() && s.getSonderwunsch().getId() == 48) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(48));
-				log.info("Special request {} removed", cbMarkiseDG.getText().replace(":", ""));
-			}
-			if (!this.cbEAntriebGaragentor.isSelected() && s.getSonderwunsch().getId() == 49) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(49));
-				log.info("Special request {} removed", cbEAntriebGaragentor.getText().replace(":", ""));
-			}
-			if (!this.cbSektionaltor.isSelected() && s.getSonderwunsch().getId() == 50) {
-				sonderwunschService.getSonderwunschHandler().removeSonderwunsch(haus,
-						sonderwunschService.getSonderwunschHandler().getSonderwunsch(50));
-				log.info("Special request {} removed", cbSektionaltor.getText().replace(":", ""));
-			}
-		}
-
-		// Füge ausgewählte Sonderwünsche hinzu
-		if (this.cbAbstellraum.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(44), haus);
-			log.info("Special request {} added", cbAbstellraum.getText().replace(":", ""));
-		}
-
-		if (this.cbVorbereitungEAntriebMarkiseEG.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(45), haus);
-			log.info("Special request {} added", cbVorbereitungEAntriebMarkiseEG.getText().replace(":", ""));
-		}
-
-		if (this.cbVortbereitungEAntriebMarkiseDG.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(46), haus);
-			log.info("Special request {} added", cbVortbereitungEAntriebMarkiseDG.getText().replace(":", ""));
-		}
-
-		if (this.cbMarkiseEG.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(47), haus);
-			log.info("Special request {} added", cbMarkiseEG.getText().replace(":", ""));
-		}
-
-		if (this.cbMarkiseDG.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(48), haus);
-			log.info("Special request {} added", cbMarkiseDG.getText().replace(":", ""));
-		}
-
-		if (this.cbEAntriebGaragentor.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(49), haus);
-			log.info("Special request {} added", cbEAntriebGaragentor.getText().replace(":", ""));
-		}
-
-		if (this.cbSektionaltor.isSelected()) {
-			sonderwunschService.getSonderwunschHandler()
-					.addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(50), haus);
-			log.info("Special request {} added", cbSektionaltor.getText().replace(":", ""));
-		}
+	private Label txtFieldAbsEG;
+	@FXML
+	private Label txtFieldVorEG;
+	@FXML
+	private Label txtFieldVorDG;
+	@FXML
+	private Label txtFieldElkEG;
+	@FXML
+	private Label txtFieldElkDG;
+	@FXML
+	private Label txtFieldElkAG;
+	@FXML
+	private Label txtFieldSekSG;
+	@FXML
+	private Label txtFieldGesamtPreis;
+	@FXML
+	private CheckBox checkBoxAbsEG;
+	@FXML
+	private CheckBox checkBoxVorEG;
+	@FXML
+	private CheckBox checkBoxVorDG;
+	@FXML
+	private CheckBox checkBoxElkEG;
+	@FXML
+	private CheckBox checkBoxElkDG;
+	@FXML
+	private CheckBox checkBoxElkAG;
+	@FXML
+	private CheckBox checkBoxSekSG;
+	private double gesamtPreis;
+	
+	public Kunde Kunde;
+	private List<Sonderwunsch> sonderwunsch;
+    private SonderwunschService sonderwunschService = new SonderwunschService();
+    private KundenService kundenService = new KundenService();
+    
+	public void init() {
+		//Preise aus der DB holen
+        sonderwunsch = sonderwunschService.getSonderwunschHandler().getSonderwunschByKategorieID(8);
+        dOpt0 = sonderwunsch.get(0).getPreis();
+        dOpt1 = sonderwunsch.get(1).getPreis();
+        dOpt2 = sonderwunsch.get(2).getPreis();
+        dOpt3 = sonderwunsch.get(3).getPreis();
+        dOpt4 = sonderwunsch.get(4).getPreis();
+        dOpt5 = sonderwunsch.get(5).getPreis();
+        dOpt6 = sonderwunsch.get(6).getPreis();
+        
+		txtFieldAbsEG.setText(dOpt0 + " €"); 
+		txtFieldVorEG.setText(dOpt1 + " €"); 
+		txtFieldVorDG.setText(dOpt2 + " €");
+		txtFieldElkEG.setText(dOpt3 + " €");
+		txtFieldElkDG.setText(dOpt4 + " €");
+		txtFieldElkAG.setText(dOpt5 + " €");
+		txtFieldSekSG.setText(dOpt6 + " €");
 		
-		Stage stage = (Stage) cbAbstellraum.getScene().getWindow();
-		stage.close();
+		checkBoxAbsEG.setSelected(false);
+		checkBoxVorEG.setSelected(false);
+		checkBoxVorDG.setSelected(false);
+		checkBoxElkEG.setSelected(false);
+		checkBoxElkDG.setSelected(false);
+		checkBoxElkAG.setSelected(false);
+		checkBoxSekSG.setSelected(false);
+		
+		checkBoxElkEG.setDisable(true); //abhaengig von VorEG
+		checkBoxElkDG.setDisable(true); //abhaengig von VorDG
+    }
+	
+	@FXML
+	void onClickCsvExport() {
+		// TODO: DB Speichern...
+		List<String> ueberschrift = new ArrayList<String>();
+		ueberschrift.add("Abstellraum auf der Terrasse des EG");
+		ueberschrift.add("Vorbereitung für elektrische Antriebe Markise EG");
+		ueberschrift.add("Vorbereitung für elektrische Antriebe Markise DG");
+		ueberschrift.add("Elektrische Markise EG");
+		ueberschrift.add("Elektrische Markise DG");
+		ueberschrift.add("Elektrischen Antrieb für das Garagentor");
+		ueberschrift.add("Sektionaltor anstatt Schwingtor für die Garage");
+		
+		List<String> attribute = new ArrayList<String>();
+		
+		if (checkBoxAbsEG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxVorEG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxVorDG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxElkEG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxElkDG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxElkAG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		if (checkBoxSekSG.isSelected())
+			attribute.add("ja");
+		else
+			attribute.add("nein");
+		
+		CSVExporter csvExport = new CSVExporter();
+		try {
+			FileWriter fw = new FileWriter(csvExport.setStrPfad(stage));
+			CSVExporter.writeLine(fw, ueberschrift);
+			CSVExporter.writeLine(fw, attribute);
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
 	}
+	
+	@FXML
+	void onClickDbSave() {
+		//44 Abstellraum auf der Terrasse des EG
+		if(checkBoxAbsEG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(44), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(44));
 
-	public void exportToCsv() throws IOException {
-		List<String> values = new ArrayList();
-		CSVExporter exporter = new CSVExporter();
-
-		if (cbAbstellraum.isSelected()) {
-			values.add("Abstellraum auf der Terrasse des EG: 3.590 Euro");
-		}
-		if (cbVorbereitungEAntriebMarkiseEG.isSelected()) {
-			values.add("Vorbereitung für elektrische Antriebe Markise EG: 170 Euro");
-		}
-		if (cbVortbereitungEAntriebMarkiseDG.isSelected()) {
-			values.add("Vorbereitung für elektrische Antriebe Markise DG: 170 Euro");
-		}
-		if (cbMarkiseEG.isSelected()) {
-			values.add("Elekrische Markise EG : 890 Euro");
-		}
-		if (cbMarkiseDG.isSelected()) {
-			values.add("Elekrische Markise DG : 890 Euro");
-		}
-		if (cbEAntriebGaragentor.isSelected()) {
-			values.add("Elektrischen Antrieb für das Garagentor: 990 Euro");
-		}
-
-		if (cbSektionaltor.isSelected()) {
-			values.add("Sektionaltor anstatt Schwingtor für die Garage: 790 Euro");
-		}
-
-		FileWriter fileWriter = new FileWriter(exporter.setStrPfad(stage));
-		CSVExporter.writeLine(fileWriter, values);
+		//45 Vorbereitung für elektrische Antriebe Markise EG
+		if(checkBoxVorEG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(45), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(45));
+		
+		//46 Vorbereitung für elektrische Antriebe Markise DG
+		if(checkBoxVorDG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(46), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(46));
+		
+		//47 Elektrische Markise EG
+		if(checkBoxElkEG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(47), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(47));
+		
+		//48 Elektrische Markise DG
+		if(checkBoxElkDG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(48), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(48));
+		
+		//49 Elektrischen Antrieb für das Garagentor
+		if(checkBoxElkAG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(49), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(49));
+		
+		//50 Sektionaltor anstatt Schwingtor für die Garage
+		if(checkBoxSekSG.isSelected())
+            sonderwunschService.getSonderwunschHandler().addSonderwunsch(sonderwunschService.getSonderwunschHandler().getSonderwunsch(50), Kunde.getHouses().get(0));
+        else
+            sonderwunschService.getSonderwunschHandler().removeSonderwunsch(Kunde.getHouses().get(0), sonderwunschService.getSonderwunschHandler().getSonderwunsch(50));
+	}
+	
+	@FXML
+	protected void OnSelect(ActionEvent event) {
+		gesamtPreis = 0;
+		//Abstellraum auf der Terrasse des EG
+		if(checkBoxAbsEG.isSelected())
+			gesamtPreis+=dOpt0;		
+		//Vorbereitung für elektrische Antriebe Markise EG
+		if(checkBoxVorEG.isSelected())
+			gesamtPreis+=dOpt1;
+		else
+			checkBoxElkEG.setDisable(true);		
+		//Vorbereitung für elektrische Antriebe Markise DG:
+		if(checkBoxVorDG.isSelected())
+			gesamtPreis+=dOpt2;
+		else
+			checkBoxElkDG.setDisable(true);		
+		//Elektrische Markise EG
+		if(checkBoxElkEG.isSelected())
+			gesamtPreis+=dOpt3;
+		//Elektrische Markise DG
+		if(checkBoxElkDG.isSelected())
+			gesamtPreis+=dOpt4;
+		//Elektrischen Antrieb für das Garagentor
+		if(checkBoxElkAG.isSelected())
+			gesamtPreis+=dOpt5;
+		//Sektionaltor anstatt Schwingtor für die Garage
+		if(checkBoxSekSG.isSelected())
+			gesamtPreis+=dOpt6;
+		txtFieldGesamtPreis.setText(gesamtPreis+ "€");
 	}
 }
